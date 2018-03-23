fa16c52917305e21823294e3a4e384bfeae50da0
Wes Bos <wesbos@gmail.com>
2018-03-22 21:12:53 -0400

    bye auth0

--- frontend/routes.js
+++
@@ -1,7 +0,0 @@
-const routes = (module.exports = require('next-routes')());
-
-routes
-  .add('about')
-  .add('item', '/item/:itemId/:slug')
-  .add('items', '/items/:page')
-  .add('order', '/order/:orderId');
