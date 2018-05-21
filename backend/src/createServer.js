603f1a0e9df2f88184cc03ce2e7177547daa0674
Johannes Schickling <schickling.j@gmail.com>
2018-05-21 16:36:32 -0700

    use prisma deploy hooks

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -19,7 +19,7 @@ function createServer() {
       db: new Prisma({
         typeDefs: 'src/generated/prisma.graphql',
         endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma DB service (value is set in variables.env)
-        secret: process.env.PRISMA_SECRET, // taken from database/prisma.yml (value is set in .env)
+        secret: process.env.PRISMA_SECRET, // taken from prisma/prisma.yml (value is set in .env)
         debug: false, // log all GraphQL queries & mutations
       }),
     }),
