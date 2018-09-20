299af12f4985080b4f5d54b31e62b467d1462e45
jameygleason <james.p.gleason@gmail.com>
2018-09-20 08:41:44 -0400

    Stepped Solutions

--- stepped-solutions/09/frontend/components/Header.js
+++ stepped-solutions/09/frontend/components/Header.js
@@ -1,6 +1,20 @@
-import Nav from './Nav';
 import Link from 'next/link';
 import styled from 'styled-components';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import Nav from './Nav';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Logo = styled.h1`
   font-size: 4rem;
