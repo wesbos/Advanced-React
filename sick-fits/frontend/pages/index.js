37a90847fe4b7733239e6e3d4d9aa552a320276f
Sergio Kopplin <sergiokopplin@gmail.com>
2018-10-12 09:37:54 -0300

    module #1 & #2

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = props => (
+	<div>
+		<p>Hey!</p>
+	</div>
+);
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sell = props => (
+	<div>
+		<p>Sell!</p>
+	</div>
+);
+
+export default Sell;
