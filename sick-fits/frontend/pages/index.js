d461c16985f812abfdde3419ed8e4b7dd3eb605e
TinaHeiligers <tina.heiligers@gmail.com>
2018-10-14 12:11:33 -0700

    Adds two basic pages with links to each other

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Home = props => (
+  <div>
+    <p>Hey!</p>
+      <Link href="/sell">
+        <a>Sell!</a>
+      </Link>
+  </div>
+);
+export default Home;
