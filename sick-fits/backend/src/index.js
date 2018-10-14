0636334c7315f6ac61f27276fa931c5f858cc434
Sergio Kopplin <sergiokopplin@gmail.com>
2018-10-14 14:21:56 -0300

    more lessons

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,14 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.start({
+	cors: {
+		credentials: true,
+		origin: process.env.FRONTEND_URL,
+	}
+}, deets => {
+	console.log(`Server is now running on port http://localhost:${deets.port}`);
+});
