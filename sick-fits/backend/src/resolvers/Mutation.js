60daaef3073f5be489848522bb65d2bdd57e081d
Tiago Brito <brito.tiagos@gmail.com>
2018-10-22 12:22:10 +1100

    Done Module 3 Server Side GraphQL

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,24 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
 
-module.exports = mutations;
+    const item = ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   //Create Dog
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
+};
+
+module.exports = Mutations;
