ff5d70c59ae1c67a513a10eaef2a4b7f7b6447c3
pixx3l <tey.taghiyev@gmail.com>
2018-10-25 16:33:46 -0400

    Lessons 1-7

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <div>
+      <p>hello from home!</p>
+    </div>
+  );
+};
+
+export default Home;
