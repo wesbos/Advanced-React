edf72f3b12fe6d43d741befb0972fcf62e15d994
Daniel Douangkesone <karateporkchop@gmail.com>
2018-11-04 09:22:54 -0600

    adding styled components

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -3,9 +3,6 @@ import Link from "next/link";
 const Home = props => (
   <div>
     <p>Hey!</p>
-    <Link href="/sell">
-      <a>Sell!</a>
-    </Link>
   </div>
 );
 
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -3,9 +3,6 @@ import Link from "next/link";
 const Sell = props => (
   <div>
     <p>Sell!</p>
-    <Link href="./index">
-      <a>Home!</a>
-    </Link>
   </div>
 );
 
