f633b3e4509f07546bb30609df81a505fbaf92be
Daniel Douangkesone <karateporkchop@gmail.com>
2018-11-23 10:31:29 -0600

    finishing Styled Components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import Meta from "./Meta";
-import styled from 'styled-components'
+import styled, {ThemeProvider, injectGlobal} from 'styled-components'
 
 const theme = {
   red: '#FF0000',
@@ -15,25 +15,55 @@ const theme = {
 
 const StyledPage = styled.div`
   background: white;
-  color: black;
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-  max-width: 1000px;
+  max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 2rem;
+`
+
+injectGlobal`
+@font-face {
+  font-family: 'radnika_next';
+  src: url('/static/radnikanext-medium-webfont.woff2')
+  format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}}
+ html {
+   box-sizing: border-box;
+   font-size: 10px;
+ }
+ *, *:before, *:after {
+   box-sizing: inherit;
+ }
+ body {
+   padding: 0;
+   margin: 0;
+   font-size: 1.5rem;
+   line-height: 2;
+ }
+ a {
+   text-decoration: none;
+   color: ${theme.black}
+ }
 `
 export default class Page extends Component {
   render() {
     return (
-      <StyledPage>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
         <Meta />
         <Header />
         <Inner>
          {this.props.children}
         </Inner>
         
-      </StyledPage>
+        </StyledPage>
+      </ThemeProvider>
+      
     );
   }
 }
