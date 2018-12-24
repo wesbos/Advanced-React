17e234adc544c9176200ee11e0834400e92ca105
Scott Schottler <scottschottler@gmail.com>
2018-12-24 18:25:47 -0500

    13 - Our first Query and Mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {}
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || []
+    // create a dog
+    const newDog = { name: args.name }
+    global.dogs.push(newDog)
+    return newDog
+  }
+}
 
 module.exports = Mutations
