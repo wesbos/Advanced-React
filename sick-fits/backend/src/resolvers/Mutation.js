e914a63dc4324d8d7e3f1c3c2474b046e0496d0f
Scott Schottler <scottschottler@gmail.com>
2018-12-24 18:54:44 -0500

    14 - Items Creation and Prisma Yoga Flow
    Module 3 Server Side GraphQL done

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,10 +1,17 @@
 const Mutations = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || []
-    // create a dog
-    const newDog = { name: args.name }
-    global.dogs.push(newDog)
-    return newDog
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    )
+
+    return item
   }
 }
 
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,8 +1,11 @@
+const { forwardTo } = require('prisma-binding')
+
 const Query = {
-  dogs(parent, args, ctx, info) {
-    global.dogs = global.dogs || []
-    return global.dogs
-  }
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items()
+  //   return items
+  // }
+  items: forwardTo('db')
 }
 
 module.exports = Query
