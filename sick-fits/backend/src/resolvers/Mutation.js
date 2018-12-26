388b8c79917badb0a95c74dca296d75d2c20f405
Scott Schottler <scottschottler@gmail.com>
2018-12-26 16:38:50 -0500

    20 - Deleting Items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -29,6 +29,16 @@ const Mutations = {
       },
       info
     )
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id }
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title}`)
+    // 2. Check if they own that item, or have the permissions
+    // TODO
+    // 3. Delete it!
+    return ctx.db.mutation.deleteItem({ where }, info)
   }
 }
 
--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -5,6 +5,7 @@ import Title from './styles/Title'
 import ItemStyles from './styles/ItemStyles'
 import PriceTag from './styles/PriceTag'
 import formatMoney from '../lib/formatMoney'
+import DeleteItem from './DeleteItem'
 
 export default class Item extends Component {
   static propTypes = {
@@ -37,7 +38,7 @@ export default class Item extends Component {
             <a>Edit ✏️</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete Item</DeleteItem>
         </div>
       </ItemStyles>
     )
