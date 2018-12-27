6addc48138cee125a158bf98ced7846d387ae962
Scott Schottler <scottschottler@gmail.com>
2018-12-27 15:47:13 -0500

    33 - Creating a Gated Sign in Component

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,8 +1,11 @@
 import CreateItem from '../components/CreateItem'
+import PleaseSignIn from '../components/PleaseSignIn'
 
 const Sell = props => (
   <div>
-    <CreateItem />
+    <PleaseSignIn>
+      <CreateItem />
+    </PleaseSignIn>
   </div>
 )
 export default Sell
