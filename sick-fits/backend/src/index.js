4fd2508046d17c9b6f709d0ad2a09401afccd9e8
Scott Schottler <scottschottler@gmail.com>
2018-12-27 13:35:25 -0500

    26 - Currently Logged In User with Middleware and Render Props

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,13 +1,23 @@
 const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 require('dotenv').config({ path: 'variables.env' })
 const createServer = require('./createServer')
 const db = require('./db')
 
 const server = createServer()
 
-// TODO use express middleware to handle cookies (JWT)
 server.express.use(cookieParser())
-// TODO use express middleware to populate current user
+
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET)
+    // put the userId onto the req for future requests to access
+    req.userId = userId
+  }
+  next()
+})
 
 server.start(
   {
--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,8 +1,16 @@
 import Link from 'next/link'
 import NavStyles from './styles/NavStyles'
+import User from './User'
 
 const Nav = () => (
   <NavStyles>
+    <User>
+      {({ data: { me } }) => {
+        console.log(me)
+        if (me) return <p>{me.name}</p>
+        return null
+      }}
+    </User>
     <Link href="/items">
       <a>Shop</a>
     </Link>
