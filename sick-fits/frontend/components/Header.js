2617a2254b24cf9ad780a3cca8658ee834716c9d
Matt Nicholas <matt@mattnicholas.io>
2019-01-05 21:37:55 -0500

    themes and styles

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,9 +1,56 @@
 import Nav from './Nav';
+import Link from 'next/link';
+import styled from 'styled-components';
+import { checkPropTypes } from 'prop-types';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+
+  a {
+    padding: 0.5rem 1rem;
+    background: ${props => props.theme.red};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`;
+
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    @media (max-wdith: 1300px){
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${props => props.theme.lightgrey};
+
+  }
+`;
 
 const Header = () => (
-  <div>
+  <StyledHeader>
     <div className="bar">
-      <a href="">Sick Fits</a>
+      <Logo>
+        <Link href="/">
+          <a>Sick Fits</a>
+        </Link>
+      </Logo>
       <Nav />
     </div>
     <div className="sub-bar">
@@ -12,7 +59,7 @@ const Header = () => (
     <div>
       Cart
     </div>
-  </div>
+  </StyledHeader>
 )
 
 export default Header;
