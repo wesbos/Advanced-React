fb287d334e0f88dd5d05e5617c1819c6bb7ae99b
davebeauchemin <37642902+davebeauchemin@users.noreply.github.com>
2019-02-21 08:09:44 -0500

    Updated

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    // Create a dog
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog;
+  }
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,8 @@
-const Query = {};
+const Query = {
+  dogs(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    return global.dogs;
+  }
+};
 
 module.exports = Query;
