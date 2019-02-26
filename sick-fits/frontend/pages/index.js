da4cf12bafdd213d40a058e6e794f40da32923e7
Kayot88 <kayot88@gmail.com>
2019-02-26 20:46:44 +0200

    initialCommit

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+const Home = props => (
+  <div>
+    <Link href="/sell">
+      <a>Sell!</a>
+    </Link>
+  </div>
+);
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+const Sell = props => (
+  <div>
+    <Link href="/">
+      <a>Home!</a>
+    </Link>
+  </div>
+);
+
+export default Sell;
