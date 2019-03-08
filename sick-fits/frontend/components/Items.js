00346eb0d1f3320057e0eeb527d4d6b68f25effd
Kayot88 <kayot88@gmail.com>
2019-03-08 18:48:29 +0200

    pagination_2

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
 import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const Center = styled.div`
   text-align: center;
@@ -18,8 +19,8 @@ const ListItems = styled.div`
 `;
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage} ) {
+    items(first:$first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -34,8 +35,13 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <Pagination page={this.props.page}/>
-        <Query query={ALL_ITEMS_QUERY}>
+        <Pagination page={this.props.page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{
+            skip: this.props.page * perPage - perPage,
+          }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>Loading...</p>;
             if (error) return <p>Error {error.message}</p>;
