d01f3923ff8b6e53f725073c0a869cec6e7f8171
parkersalterego <parkersalterego92@gmail.com>
2019-04-23 13:37:09 -0700

    feat(Header.js): styled component Logo added and styled

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,9 +1,36 @@
 import Nav from "./Nav";
+import Link from "next/link";
+import styled from "styled-components";
+
+const Logo = styled.h1`
+    font-size: 4rem;
+    margin-left: 2rem;
+    position: relative;
+    z-index: 2;
+    transform: skew(-7deg);
+
+    a {
+        padding: .5rem 1rem;
+        background: ${props => props.theme.red};
+        color: #FFFFFF;
+        text-transform: uppercase;
+        text-decoration: none;
+    }
+
+    @media(max-width: 1300px) {
+        margin: 0;
+        text-align: center;
+    }
+`;
 
 const Header = () => (
     <div>
         <div className="bar">
-            <a href="/">Sick Fits</a>
+            <Logo>
+                <Link href="/">
+                    <a>Sick Fits</a>
+                </Link>
+            </Logo>
             <Nav/>
         </div>
         <div className="sub-bar">
