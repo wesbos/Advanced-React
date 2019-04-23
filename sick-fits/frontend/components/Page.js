52684b3bb860be090ef65e5f474b3693a3ab651c
parkersalterego <parkersalterego92@gmail.com>
2019-04-23 13:29:55 -0700

    feat(Page.js): added styled components for StyledPage and Inner and added ThemeProvider

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import Header from "./Header";
 import Meta from "./Meta";
+import styled, { ThemeProvider, InjectGlobal } from "styled-components";
+
+const theme = {
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rggba(0, 0, 0, .99)"
+};
+
+const StyledPage = styled.div`
+  background: #FFFFFF;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 class Page extends React.Component {
   render() {
     return (
-      <div>
-        <Meta/>
-        <Header/>
-        { this.props.children }
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta/>
+          <Header/>
+          <Inner>
+            { this.props.children }
+          </Inner>
+        </StyledPage>
+      </ThemeProvider>
     )
   }
 }
