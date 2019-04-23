db2b837562babfc7435dbe0128d131d0e0c422e7
parkersalterego <parkersalterego92@gmail.com>
2019-04-23 13:43:08 -0700

    feat(Header.js): styled component StyledHeader added and styled

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -23,8 +23,29 @@ const Logo = styled.h1`
     }
 `;
 
+const StyledHeader = styled.header`
+    .bar {
+        border-bottom: 10px solid ${props => props.theme.black};
+        display: grid;
+        grid-template-columns: auto 1fr;
+        justify-content: space-between;
+        align-items: stretch;
+
+        @media(max-width: 1300px) {
+            grid-template-columns: 1fr;
+            justify-content: center;
+        }
+    }
+
+    .sub-bar {
+        display: grid;
+        grid-template-columns: 1fr auto;
+        border-bottom: 1px solid ${props => props.theme.lightgrey};
+    }
+`;
+
 const Header = () => (
-    <div>
+    <StyledHeader>
         <div className="bar">
             <Logo>
                 <Link href="/">
@@ -37,7 +58,7 @@ const Header = () => (
             <p>Search</p>
         </div>
         <div>Cart</div>
-    </div>
+    </StyledHeader>
 )
 
 export default Header;
