0071a83233582c5a5cf630c93a83367bca36c349
Jeremy Levett <jlevett@engineer.com>
2019-06-27 14:00:33 +1000

    feat: item creation and yogo flow setup

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,27 @@
-const Query = {};
+const { forwardTo } = require ('prisma-binding');
+const Query = {
+    /*
+    *EXAMPLE USING GLOBAL SCOPE
+    dogs(parent, args, ctx, info) {
+            global.dogs = global.dogs || [];
+            return global.dogs;
+        },
+    */
+   /*
+    Since the below is an example of a predefined query from 
+    the schema.graphql we can use 'forwardTo' to get this query
+    without writting it below.
+
+    async items(parent, args, ctx, info){
+        console.log('getting items');
+        const items = await ctx.db.query.items();
+        return items;
+    }
+
+    USE THIS INSTEAD "items: forwardTo('db'),"
+    */
+   items: forwardTo('db'), 
+};
 
 module.exports = Query;
+
