69eea27d0aef6b22679238dc1e33c55b31233c67
danwventura <danwventura@gmail.com>
2019-07-07 23:20:41 -0500

    nav styles, routing for two links, fixing flash of unstyled content

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,14 +1,26 @@
-import Link from "next/link";
+import Link from "next/link"; 
+import NavStyles from "./styles/NavStyles";
 
 const Nav = () => (
-	<div>
+	<NavStyles>
+		
+		<Link href="/items">
+				<a>Items</a>
+		</Link>
 		<Link href="/sell">
-				<a>Sell!</a>
+				<a>Sell</a>
 		</Link>
-		<Link href="/">
-				<a>Home!</a>
+		<Link href="/signup">
+				<a>Sign Up</a>
 		</Link>
-	</div>
+		<Link href="/orders">
+				<a>Orders</a>
+		</Link>
+		<Link href="/me">
+				<a>My Account</a>
+		</Link>
+
+	</NavStyles>
 );
 
 export default Nav;
