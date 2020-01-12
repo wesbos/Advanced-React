e5dd9b2117456bd1685851061d3830c5758cf03f
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2020-01-11 20:51:16 -0600

    Working backend with docker-compose hot reloaded

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,29 +1,17 @@
 // let's go!
+'use strict';
+
 const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
 const app = express();
-const router = express.Router();
-
-const path = __dirname + '/views/';
-const port  = 8080;
-
-
-router.use(function (req,res,next) {
-	  console.log('/' + req.method);
-	  next();
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-router.get('/', function(req,res){
-	  res.sendFile(path + 'index.html');
-});
-
-router.get('/sharks', function(req,res){
-	  res.sendFile(path + 'sharks.html');
-});
-
-
-app.use(express.static(path));
-app.use('/', router);
-
-app.listen(port, function () {
-	  console.log('Example app listening on port 8080!')
-})
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
