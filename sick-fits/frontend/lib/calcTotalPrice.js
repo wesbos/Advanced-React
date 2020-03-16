32cf8d9cceee3dac2471174b6ea1cd16086f5538
Marcus Rapacioli <marcus.rapacioli@gmail.com>
2020-03-16 11:50:31 +0000

    backend works

--- sick-fits/frontend/lib/calcTotalPrice.js
+++
@@ -1,6 +0,0 @@
-export default function calcTotalPrice(cart) {
-  return cart.reduce((tally, cartItem) => {
-    if (!cartItem.item) return tally;
-    return tally + cartItem.quantity * cartItem.item.price;
-  }, 0);
-}
--- stepped-solutions/13/backend/src/resolvers/Mutation.js
+++
@@ -1,3 +0,0 @@
-const Mutations = {};
-
-module.exports = Mutations;
