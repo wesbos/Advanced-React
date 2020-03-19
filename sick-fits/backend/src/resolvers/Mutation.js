f335e5c1cd9ba0c9cdaddcdc638bfb01ca41726c
Tayt Low <nictayt@gmail.com>
2020-03-18 21:54:33 -0600

    updated datamodel.graphql with an item interface, fixed prisma schema error

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const Mutation = {};
+const Mutation = {
+    createDog(parent, args, ctx, info) {
+        global.dogs = global.dogs || [];
+        const newDog = {name: args.name};
+        global.dogs.push(newDog)
+        return newDog
+    }
+};
 
 module.exports = Mutation;
