751d0fda8bfea013210a01af665a3571a0a7f3f2
Kesa Shabarish <shabarish.shabbi@gmail.com>
2021-02-06 12:45:24 -0500

    Header and Nav componets created

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,9 @@
+import Header from './Header';
+
 export default function Page({ children }) {
     return (
         <div>
+            <Header></Header>
             <h2>This is Page Component</h2>
             {children}
         </div>
