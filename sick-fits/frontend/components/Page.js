2d5f59839f0552ce74227cfe534e9cee3b0cc8d5
dnodolf1212 <dnodolf@gmail.com>
2021-03-22 23:24:21 -0700

    gobal styles set in Page.js, styled.div still buggy but will correct tomorrow

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,13 +1,71 @@
 import PropTypes from 'prop-types';
 import Header from './Header';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+
+    @fontface {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2')
+    format('woff2');
+    font-weight: normal;
+    font-style: noirmal;
+  }
+
+  html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(---lightGray);
+    offWhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba()0,0,0,0.09
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, Oxygen, Ubuntum Cantarell, 'Open Sans', 
+    'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(---black);
+  }  
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  button {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, Oxygen, Ubuntum Cantarell, 'Open Sans', 
+    'Helvetica Neue',
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default function Page({children}) {
   return ( 
     <div>
+      <GlobalStyles />
       <Header />
-      <h3>HEY!!!PAGE COMPONENT HERE!!!!</h3>
-      {children}
-      
+      <InnerStyles>{children}</InnerStyles>    
     </div>
   );
 }
