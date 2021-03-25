f220ca0ede6bab8c2b8323a995141461355e2087
Brendan Gassler <bgassler4@gmail.com>
2021-03-25 16:43:13 +1100

    Header styles

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export default function Nav() {
   return (
     <nav>
-      <Link href="products">Products</Link>
-      <Link href="sell">Sell</Link>
-      <Link href="orders">Orders</Link>
-      <Link href="account">Account</Link>
+      <Link href="/products">Products</Link>
+      <Link href="/sell">Sell</Link>
+      <Link href="/orders">Orders</Link>
+      <Link href="/account">Account</Link>
     </nav>
   );
 }
