082e7e34b45a6ebb42afbfbf4a7d6b355be83be0
Brendan Gassler <bgassler4@gmail.com>
2021-03-26 13:13:16 +1100

    fix flicker when rendering

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -3,53 +3,43 @@ import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
 
 const GlobalStyles = createGlobalStyle`
- @font-face {
-   font-family: 'radnika_next';
-   src: url('/static/radnikanext-medium-webfont.woff2')
-   format('woff2');
-   font-weight: normal;
-   font-style: normal;
- }
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
   html {
     --red: #ff0000;
     --black: #393939;
-    --grey: #3a3a3a;
+    --grey: #3A3A3A;
     --gray: var(--grey);
     --lightGrey: #e1e1e1;
-    --lightGray: var(--lightGrey);
+    --lightGray: var(---lightGrey);
     --offWhite: #ededed;
     --maxWidth: 1000px;
     --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
     box-sizing: border-box;
   }
   *, *:before, *:after {
-    box-sizing: inherit
+    box-sizing: inherit;
   }
   body {
-    font-family: 'radnika_next' --apple-system,
-    BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
-    sans-serif;
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     padding: 0;
     margin: 0;
     font-size: 1.5rem;
-    line-height: 2;
-
+    line-height:2;
   }
-
   a {
     text-decoration: none;
-    color: var(--black);
+    color: var(---black);
   }
-
   a:hover {
     text-decoration: underline;
   }
-
   button {
-    font-family: 'radnika_next' --apple-system,
-    BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
   }
 `;
 
