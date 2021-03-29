9c432d5232403dd27bdc17b49213ea5c8bb6d6a0
Brendan Gassler <bgassler4@gmail.com>
2021-03-29 14:06:12 +1100

    getting keystone running

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseUrl =
+    process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+    maxAge: 60 * 60 * 24 * 360, //how long do users stay logged in
+    cookieSecret: process.env.COOKIE_SECRET
+}
+
+export default config({
+    server: {
+        cors: {
+            origin: [process.env.FRONTEND_URL],
+            credentials: true
+        }
+    },
+    db: {
+        adapter: 'mongoose',
+        url: databaseUrl,
+        // TODO: add data seeding here
+    },
+    lists: createSchema({
+        //Schema goes here
+    }),
+    ui: {
+        //TODO: change this when implementing roles
+        isAccessAllowed: () => true
+    },
+    //TODO: Add session values here 
+});
