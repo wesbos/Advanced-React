20b60e5126981f3b4150197d61c52bfc118668b8
Brendan Gassler <bgassler4@gmail.com>
2021-03-30 17:11:42 +1100

    Upload Product images to Cloudinary via backend

--- sick-fits/backend/schemas/User.ts
+++ sick-fits/backend/schemas/User.ts
@@ -1,12 +1,12 @@
-import { list } from "@keystone-next/keystone/schema";
+import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields';
 
 export const User = list({
-    //access:
-    //ui:
-    fields: {
-        name: text({ isRequired: true }),
-        email: text({ isRequired: true, isUnique: true }),
-        password: password(),
-    },
-})
+  // access:
+  // ui:
+  fields: {
+    name: text({ isRequired: true }),
+    email: text({ isRequired: true, isUnique: true }),
+    password: password(),
+  },
+});
