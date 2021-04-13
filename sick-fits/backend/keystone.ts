862e975a86b35277a5310aba44717dfa254d3460
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-04-12 22:43:17 -0400

    WIP: Added Auth To Application

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,5 +1,7 @@
+import { createAuth } from '@keystone-next/auth'
 import { config, createSchema } from '@keystone-next/keystone/schema'
 import 'dotenv/config';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session'
 import { User } from './schemas/User';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial'
@@ -9,7 +11,17 @@ const sessionConfig = {
     secret: process.env.COOKIE_SECRET
 }
 
-export default config({
+const { withAuth } = createAuth({
+    listKey: 'User',
+    identityField: 'email',
+    secretField: 'password',
+    initFirstItem: {
+        fields: ['name', 'email', 'password'],
+        // TODO: Add in initial data
+    }
+});
+
+export default withAuth(config({
     server: {
         cors: {
             origin: [process.env.FRONTEND_URL],
@@ -26,7 +38,13 @@ export default config({
         User: User
     }),
     ui: {
-        // TODO: change this for roles
-        isAccessAllowed: () => true,
-    }
-});
+        // Show the UI only for people who pass this test
+        isAccessAllowed: ({ session }) => {
+            return !!session?.data;
+
+        }
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+        User: 'id name email'
+    })
+}));
