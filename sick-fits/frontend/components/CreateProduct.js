0c6510ba745966c4a56a4dbdaeeba532904f71e3
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-04-18 23:13:18 -0400

    WIP: Added image field for submit form.

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,39 +1,46 @@
-import { useState } from "react"
 import useForm from "../lib/useForm";
+import Form from './styles/Form'
 
 export default function CreateProduct() {
     const {inputs, handleChange, resetForm, clearForm} = useForm({
+        image: '',
         name: "Nike",
         price: 34234,
         description: "Nice Shoes"
     });
     return (
-        <form>
-            <label htmlFor="name">
-                Name
-                <input type="text" name="name" id="name" placeholder="Name" value={inputs.name}
-                    onChange={handleChange}
-                />
-            </label>
-            <label htmlFor="price">
-                Price
-                <input type="number" name="price" id="price" placeholder="Price" value={inputs.price}
-                    onChange={handleChange}
-                />
-            </label>
-            <label htmlFor="description">
-                Description
-                <input type="text" name="description" id="description" placeholder="Description" value={inputs.description}
-                    onChange={handleChange}
-                />
-            </label>
+        <Form onSubmit={(e) => {
+            e.preventDefault();
+            console.log(inputs)
+        }}>
+            <fieldset>
+                <label htmlFor="image">
+                    Name
+                    <input required type="file" name="image" id="image" value={inputs.image}
+                        onChange={handleChange}
+                        />
+                </label>
+                <label htmlFor="name">
+                    Name
+                    <input type="text" name="name" id="name" placeholder="Name" value={inputs.name}
+                        onChange={handleChange}
+                        />
+                </label>
+                <label htmlFor="price">
+                    Price
+                    <input type="number" name="price" id="price" placeholder="Price" value={inputs.price}
+                        onChange={handleChange}
+                        />
+                </label>
+                <label htmlFor="description">
+                    Description
+                    <textarea name="description" id="description" placeholder="Description" value={inputs.description}
+                        onChange={handleChange}
+                        />
+                </label>
 
-            <button type="button" onClick={clearForm}>
-                Clear Form
-            </button>
-            <button type="button" onClick={resetForm}>
-                Reset Form
-            </button>
-        </form>
+                <button type="Submit">Add Product</button>
+            </fieldset>
+        </Form>
     )
 }
--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -10,7 +10,7 @@ export default function useForm(initial = {}) {
             value = parseInt(value);
         }
         if(type === 'file') {
-            value[0] = e.target.files;
+            [value] = e.target.files;
         }
         setInputs({
             ...inputs,
