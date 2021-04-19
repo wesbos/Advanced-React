ffc1bcbdc261932fd05db318132a5d78482ce064
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-04-18 23:45:25 -0400

    WIP: createProduct Mutation

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,5 +1,37 @@
+import gql from 'graphql-tag'
+import { useMutation } from '@apollo/client'
+import DisplayError from './ErrorMessage'
 import useForm from "../lib/useForm";
 import Form from './styles/Form'
+
+const CREATE_PRODUCT_MUTATION = gql`
+    mutation CREATE_PRODUCT_MUTATION(
+        # Which variables are getting passed in? And what types are they?
+        $name: String!
+        $description: String!
+        $price: Int!
+        $image: Upload
+    ) {
+    createProduct(
+        data:{
+            name: $name
+            description: $description
+            price: $price
+            status: "AVAILABLE"
+            photo: {
+                create: {
+                    image: $image,
+                    altText: $name
+                }
+            }
+    }) {
+        id
+        price
+        description
+        name
+    }
+}
+`
 
 export default function CreateProduct() {
     const {inputs, handleChange, resetForm, clearForm} = useForm({
@@ -8,17 +40,22 @@ export default function CreateProduct() {
         price: 34234,
         description: "Nice Shoes"
     });
+    const [createProduct, { loading, error, data }] = useMutation(CREATE_PRODUCT_MUTATION, {
+        variables: inputs
+    })
     return (
-        <Form onSubmit={(e) => {
+        <Form onSubmit={async (e) => {
             e.preventDefault();
             console.log(inputs)
+            // Submit the inputfields to the backend:
+            const res = await createProduct();
+            console.log(res)
         }}>
-            <fieldset>
+            <DisplayError error={error} />
+            <fieldset disabled={loading} aria-busy={loading}>
                 <label htmlFor="image">
-                    Name
-                    <input required type="file" name="image" id="image" value={inputs.image}
-                        onChange={handleChange}
-                        />
+                    Image
+                    <input required type="file" name="image" id="image" onChange={handleChange} />
                 </label>
                 <label htmlFor="name">
                     Name
--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -10,7 +10,7 @@ export default function useForm(initial = {}) {
             value = parseInt(value);
         }
         if(type === 'file') {
-            [value] = e.target.files;
+            value = e.target.files[0];
         }
         setInputs({
             ...inputs,
