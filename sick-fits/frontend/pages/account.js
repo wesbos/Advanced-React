3ebda855a579f61c5a91044e3389398208156dba
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-07-07 14:23:44 -0400

    intro to next.js

---
+++ sick-fits/frontend/pages/account.js
@@ -0,0 +1,5 @@
+export default function AccountPage() {
+    return <div>
+        <p>Hello!</p>
+    </div>
+}
---
+++ sick-fits/frontend/pages/orders.js
@@ -0,0 +1,5 @@
+export default function OrdersPage() {
+    return <div>
+        <p>Hello!</p>
+    </div>
+}
