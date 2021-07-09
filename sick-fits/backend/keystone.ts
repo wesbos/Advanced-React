8db6ae1972e6066471fee8295b7ae838c42516c6
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-07-09 16:16:03 -0400

    create User schema

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -23,6 +24,7 @@ export default config({
     },
     lists: createSchema({
         // Schema items go in here
+        User: User
     }),
     ui: {
         // TODO: change this for roles
