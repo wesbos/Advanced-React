f6b106598a89d7d79a24ace5895978f16e4d20b7
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-07-09 17:33:12 -0400

    session data

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
 import { User } from './schemas/User';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session'
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -9,7 +11,18 @@ const sessionConfig = {
     secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
+const { withAuth } = createAuth({
+    listKey: 'User',
+    identityField: 'email',
+    secretField: 'password',
+    initFirstItem: {
+        fields: ['name', 'email', 'password'],
+        // TODO: add initial roles here
+    }
+});
+
+export default withAuth(
+    config({
     server: {
         cors: {
             origin: [process.env.FRONTEND_URL],
@@ -27,8 +40,15 @@ export default config({
         User: User
     }),
     ui: {
-        // TODO: change this for roles
-        isAccessAllowed: () => true,
+        // show the UI only for people who pass this test:
+        isAccessAllowed: ({ session }) => {
+            console.log(session);
+            return !!session?.data;
+        },
     },
+    session: withItemData(statelessSessions(sessionConfig), {
+        User: `id`
+    })
     // TODO: add session values here
-});
+    })
+);
