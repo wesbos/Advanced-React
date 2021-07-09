5b0dda7ff1d7aed4d4a6cf30d8d8b7c0c7e22858
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-07-09 09:10:35 -0400

    nprogress

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,15 @@
 import Page from '../components/Page'
+// NProgress.js: a nanoscopic progress bar
+import NProgress from 'nprogress';
+import Router from 'next/router';
+// todo: swap with our own
+// import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start() );
+Router.events.on('routeChangeComplete', () => NProgress.done() );
+Router.events.on('routeChangeError', () => NProgress.done())
+
 
 export default function MyApp({ Component, pageProps }) {
     return (
--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -3,7 +3,7 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-            {/* <Head></Head> */}
+            <Head></Head>
             <body>
                 <Main />
                 <NextScript />
