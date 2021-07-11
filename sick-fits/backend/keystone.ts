2a394b6dd193b429233de8d823b8975a31cb981b
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-07-11 10:31:54 -0400

    seed data

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -5,6 +5,7 @@ import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
 import { withItemData, statelessSessions } from '@keystone-next/keystone/session'
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -35,7 +36,12 @@ export default withAuth(
     db: {
         adapter: 'mongoose',
         url: databaseURL,
-        // TODO: add data seeding here
+        async onConnect(keystone) {
+            console.log('Connected to the database!');
+            if(process.argv.includes('--seed-data')) {
+                await insertSeedData(keystone);
+            }
+        },
     },
     lists: createSchema({
         // Schema items go in here
