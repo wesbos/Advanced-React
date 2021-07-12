a537b7cc32a89fda8a2d7765b60d0cd7a2227a5a
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-07-12 12:41:47 -0400

    navstyles, headerstyles fix

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -23,7 +23,7 @@ const HeaderStyles = styled.header`
         display: grid;
         grid-template-columns: auto 1fr;
         justify-content: space-between;
-        align-items: center;
+        align-items: stretch;
     }
 
     .sub-bar {
@@ -42,11 +42,12 @@ export default function Header() {
                         Sick fits
                     </Link>
                 </Logo>
+                <Nav />
             </div>
             <div className="sub-bar">
                 <p>Search</p>
             </div>
-            <Nav />
+            
         </HeaderStyles>
     )
 }
