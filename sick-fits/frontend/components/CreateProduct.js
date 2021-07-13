f560f5d007659af5daf8c2c344e8d1a54aabeb63
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-07-13 12:44:04 -0400

    styled form

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,36 +1,67 @@
 import useForm from "../lib/useForm";
+import Form from './styles/Form';
 
 export default function CreateProduct() {
     const { inputs, handleChange, clearForm, resetForm } = useForm({
+        image: '',
         name: 'These Shoes are $300',
         price: 30000,
         description: "Kelly",
     });
     return (
-        <form>
-            <label htmlFor="name">
-                <input 
-                    type="text" 
-                    id="name" 
-                    name="name" 
-                    placeholder="Name"
-                    value={inputs.name} 
-                    onChange={handleChange}
-                    />
-            </label>
-            <label htmlFor="price">
-                <input 
-                    type="number" 
-                    id="price" 
-                    name="price" 
-                    placeholder="Price"
-                    value={inputs.price} 
-                    onChange={handleChange}
-                    />
-            </label>
-            <button type="button" onClick={clearForm}>Clear Form</button>
-            <button type="button" onClick={resetForm}>Reset Form</button>
-        </form>
+        <Form onSubmit={(e) => {
+            e.preventDefault();
+            console.log(inputs);
+        }}>
+            {/* see Form.js style for aria-busy (accessibility attribute that serves as a loading bar) */}
+            <fieldset aria-busy>
+                <label htmlFor="image">
+                    Image
+                    <input 
+                        type="file" 
+                        id="image" 
+                        name="image"
+                        onChange={handleChange}
+                        />
+                </label>
+
+                <label htmlFor="name">
+                    Name
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        placeholder="Name"
+                        value={inputs.name} 
+                        onChange={handleChange}
+                        />
+                </label>
+                <label htmlFor="price">
+                    Price
+                    <input 
+                        type="number" 
+                        id="price" 
+                        name="price" 
+                        placeholder="Price"
+                        value={inputs.price} 
+                        onChange={handleChange}
+                        />
+                </label>
+                <label htmlFor="description">
+                    Description
+                    <textarea 
+                        id="description" 
+                        name="description" 
+                        placeholder="Name"
+                        value={inputs.description} 
+                        onChange={handleChange}
+                        />
+                </label>
+                {/* <button type="button" onClick={clearForm}>Clear Form</button>
+                <button type="button" onClick={resetForm}>Reset Form</button> */}
+                <button type="submit">+ Add Product</button>
+            </fieldset>
+        </Form>
     )
 
 }
--- sick-fits/frontend/lib/useForm.js
+++ sick-fits/frontend/lib/useForm.js
@@ -9,7 +9,7 @@ export default function useForm(initial = {}) {
             value = parseInt(value);
         };
         if(type = 'file') {
-            value[0] = e.target.files;
+            [value] = e.target.files;
         };
         setInputs({
             // copy the existing state:
