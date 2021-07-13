23baf8766a565554befb15ebd9929379cbab3045
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-07-13 15:32:02 -0400

    graphQL mutations for CreateProduct

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,5 +1,33 @@
 import useForm from "../lib/useForm";
 import Form from './styles/Form';
+import gql from 'graphql-tag';
+import DisplayError from './ErrorMessage';
+import { useMutation } from "@apollo/client";
+
+const CREATE_PRODUCT_MUTATION = gql`
+    mutation CREATE_PRODUCT_MUTATION(
+        # Which variables are getting passed in? And what types are they?
+        $name: String!
+        $description: String!
+        $price: Int!
+        $image: Upload
+        )   {
+        createProduct(
+            data: {
+                name: $name
+                description: $description
+                price: $price
+                status: "AVAILABLE"
+                photo: { create: { image: $image, altText: $name } }
+            }
+         ) {
+            id
+            price
+            description
+            name
+        }
+    }
+`;
 
 export default function CreateProduct() {
     const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -8,13 +36,22 @@ export default function CreateProduct() {
         price: 30000,
         description: "Kelly",
     });
+
+    const [createProduct, { loading, error, data }] = useMutation(CREATE_PRODUCT_MUTATION, {
+        variables: inputs,
+    });
+    // console.log(createProduct);
     return (
-        <Form onSubmit={(e) => {
+        <Form onSubmit={async (e) => {
             e.preventDefault();
-            console.log(inputs);
+            await createProduct();
+            console.log(res);
+            clearForm();
+            
         }}>
             {/* see Form.js style for aria-busy (accessibility attribute that serves as a loading bar) */}
-            <fieldset aria-busy>
+            <DisplayError error={error}/>
+            <fieldset disabled={loading} aria-busy={loading}>
                 <label htmlFor="image">
                     Image
                     <input 
