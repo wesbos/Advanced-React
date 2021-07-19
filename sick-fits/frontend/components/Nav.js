621ea81fce43ea0fe783921fe7ca3c990c487368
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-07-19 15:59:21 -0400

    query current User, set up signin

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -2,12 +2,24 @@ import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
  
 export default function Nav() {
+    const user = useUser();
     return (
         <NavStyles>
             <Link href="/products">Products</Link>
-            <Link href="/sell">Sell</Link>
-            <Link href="/orders">Orders</Link>
-            <Link href="/account">Account</Link>
+            {/* if there is a user (i.e. we're logged in) display these 3 Link tags... */}
+            { user && (
+                <>
+                <Link href="/sell">Sell</Link>
+                <Link href="/orders">Orders</Link>
+                <Link href="/account">Account</Link>
+                </>
+            )}
+            { !user && (
+                <>
+                <Link href="/signin">Sell</Link>
+                </>
+            )}
+            
         </NavStyles>
     )
 }
---
+++ sick-fits/frontend/pages/signin.js
@@ -0,0 +1,8 @@
+export default function SignInPage() {
+    return (
+        <div>
+            <p>Sign in!</p>
+        </div>
+        
+    )
+}
