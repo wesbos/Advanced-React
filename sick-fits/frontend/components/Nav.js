447190a8070896af9f1010650650ad668cd47aa3
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-07-20 11:56:09 -0400

    create SignOut component w/ mutation

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import SignOut from './SignOut';
  
 export default function Nav() {
     const user = useUser();
@@ -12,6 +13,7 @@ export default function Nav() {
                 <Link href="/sell">Sell</Link>
                 <Link href="/orders">Orders</Link>
                 <Link href="/account">Account</Link>
+                <SignOut />
                 </>
             )}
             { !user && (
