6e223c1ae50fcc0f83e73c23ef21fb42cdfdc718
ffewster <ffewster@twigeducation.com>
2021-08-12 15:43:19 +0100

    Chapter 33 - Pagination dynamic routing

--- sick-fits/frontend/pages/products/index.jsx
+++ sick-fits/frontend/pages/products/index.jsx
@@ -1,12 +1,15 @@
+import { useRouter } from 'next/dist/client/router';
 import Pagination from '../../components/Pagination';
 import ProductsList from '../../components/Products/Products';
 
 export default function Product() {
+  const { query } = useRouter();
+  const page = parseInt(query.page);
   return (
     <div>
-      <Pagination pageNumber={1} />
+      <Pagination pageNumber={page || 1} />
       <ProductsList />
-      <Pagination pageNumber={1} />
+      <Pagination pageNumber={page || 1} />
     </div>
   );
 }
