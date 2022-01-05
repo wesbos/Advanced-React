4b3468b14d78dcf21ce02227dd81f0494151ddd3
Ben Cawrse <3941017+bcawrse@users.noreply.github.com>
2022-01-05 10:25:52 -0500

    Replace getInitialProps w/ Vercel version
    Replaced getInitialProps with Vercel NextJS Styled Components version. 
    - https://github.com/vercel/next.js/blob/canary/examples/with-styled-components/pages/_document.js

--- finished-application/frontend/pages/_document.js
+++ finished-application/frontend/pages/_document.js
@@ -2,13 +2,30 @@ import Document, { Html, Head, NextScript, Main } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const sheet = new ServerStyleSheet();
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />)
-    );
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
