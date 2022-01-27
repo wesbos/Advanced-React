4075ce500b735cb3b1ffc7d04b3e10547e9318de
David Coelho <david.coelho@xing.com>
2022-01-27 21:52:42 +0000

    feat: FE - fixing styled components flicker on server render

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export default function Nav() {
   return (
     <nav>
-      <Link href="./account">Account</Link>
-      <Link href="./orders">Orders</Link>
-      <Link href="./products">Products</Link>
-      <Link href="./sell">Sell</Link>
+      <Link href="/account">Account</Link>
+      <Link href="/orders">Orders</Link>
+      <Link href="/products">Products</Link>
+      <Link href="/sell">Sell</Link>
     </nav>
   );
 }
