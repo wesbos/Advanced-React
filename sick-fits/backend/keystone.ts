2306323357672024191d7da82562ccf0e28abe01
David Coelho <david.coelho@xing.com>
2022-02-20 11:51:49 +0000

    feat: feat: BE server side graphql development
    *10 - setting up DB
    
    * 12 - setting up keystone and TS
    
    * 13 - creating
    first user
    
    * 14 - adding auth to the app
    
    * 15 - creating products data
    type

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,55 @@
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { Product } from './schemas/Product';
+import { User } from './schemas/User';
+import 'dotenv/config';
+
+const databaseUrl =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // how long they stay signed in
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: add in initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseUrl,
+      // TODO: add data seeding here
+    },
+    lists: createSchema({
+      // Schema types go in here
+      User,
+      Product,
+    }),
+    ui: {
+      // Show the UI only for people that pass this test
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id name email',
+    }),
+  })
+);
