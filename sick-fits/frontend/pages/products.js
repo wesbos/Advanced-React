561ba5d6f879258a901428fcf0ae7dd8cb5e6ef8
David Coelho <davidcoelho02@outlook.com>
2022-05-02 11:05:21 +0100

    feat: pagination Links - 32

--- sick-fits/frontend/pages/products.js
+++ sick-fits/frontend/pages/products.js
@@ -1,9 +1,12 @@
+import Pagination from '../components/Pagination';
 import Products from '../components/Products';
 
 export default function ProductsPage() {
   return (
     <div>
+      <Pagination page={1} />
       <Products />
+      <Pagination page={1} />
     </div>
   );
 }
