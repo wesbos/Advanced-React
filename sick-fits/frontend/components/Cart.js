be0db5f5731e07985972794c4aa442a1e160577f
David Coelho <davidcoelho02@outlook.com>
2022-05-05 11:30:48 +0100

    feat: cart - Remove From Cart Button - 49

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -6,6 +6,7 @@ import { useUser } from './User';
 import formatMoney from '../lib/formatMoney';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import { useCart } from '../lib/cartState';
+import RemoveFromCart from './RemoveFromCart';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -22,13 +23,14 @@ const CartItemStyles = styled.li`
 `;
 
 function CartItem({ cartItem }) {
-  const { product } = cartItem;
+  const { product, id } = cartItem;
 
   if (!product) return null;
 
   return (
     <CartItemStyles>
       <img
+        height="100"
         width="100"
         src={product.photo.image.publicUrlTransformed}
         alt={product.name}
@@ -42,6 +44,7 @@ function CartItem({ cartItem }) {
           </em>
         </p>
       </div>
+      <RemoveFromCart id={id} />
     </CartItemStyles>
   );
 }
