406afa82580c094512f497bca3f318c3b4c401c4
David Coelho <davidcoelho02@outlook.com>
2022-05-22 15:52:19 +0100

    fix: not able to use API key for payment STRIPE

--- sick-fits/backend/lib/stripe.ts
+++ sick-fits/backend/lib/stripe.ts
@@ -1,4 +1,5 @@
 import Stripe from 'stripe';
+import 'dotenv/config';
 
 const stripeConfig = new Stripe(process.env.STRIPE_SECRET || '', {
   apiVersion: '2020-08-27',
--- sick-fits/backend/mutations/checkout.ts
+++ sick-fits/backend/mutations/checkout.ts
@@ -1,16 +1,10 @@
-import {
-  CartItemCreateInput,
-  OrderCreateInput,
-} from '../.keystone/schema-types';
-/* eslint-disable */
-
 import { KeystoneContext } from '@keystone-next/types';
+import { OrderCreateInput } from '../.keystone/schema-types';
 import stripeConfig from '../lib/stripe';
 
-
 interface Arguments {
-  token: string
-} 
+  token: string;
+}
 
 const graphql = String.raw;
 
@@ -18,18 +12,18 @@ async function checkout(
   root: any,
   { token }: Arguments,
   context: KeystoneContext
-  ): Promise<OrderCreateInput> {
-    // 1. make sure they are signed in
-    const userId = context.session.itemId;
-    
-    if(!userId) {
-      throw new Error('Sorry! You must be signed in to create an order!');
-    }
-    
-    // 1.2. Query the user
-    const user = await context.lists.User.findOne({
-      where: { id: userId },
-      resolveFields: graphql`
+): Promise<OrderCreateInput> {
+  // 1. make sure they are signed in
+  const userId = context.session.itemId;
+
+  if (!userId) {
+    throw new Error('Sorry! You must be signed in to create an order!');
+  }
+
+  // 1.2. Query the user
+  const user = await context.lists.User.findOne({
+    where: { id: userId },
+    resolveFields: graphql`
       id
       name
       email 
@@ -50,30 +44,39 @@ async function checkout(
           } 
         }
       }
-      `
-    });
-    console.dir(user, {depth: null})
-    // 2. calculate the total price fot their order
-    const cartItems = user.cart.filter((cartItem) => cartItem.product);
-    const amount = cartItems.reduce(function(tally: number, cartItem: CartItemCreateInput) {
-      return tally + cartItem.quantity * cartItem.product.price;
-    }, 0)
+      `,
+  });
+  console.dir(user, { depth: null });
+  // 2. calculate the total price fot their order
+  const cartItems = user.cart.filter((cartItem) => cartItem.product);
+  const amount = cartItems.reduce(function (
+    tally: number,
+    cartItem: CartItemCreateInput
+  ) {
+    return tally + cartItem.quantity * cartItem.product.price;
+  },
+  0);
 
-    console.log('amount', amount)
-    // 3. create the payment with the stripe library 
-    const charge = await stripeConfig.paymentIntents.create({
+  console.log('amount', amount, '; token', token);
+  // 3. create the payment with the stripe library
+  const charge = await stripeConfig.paymentIntents
+    .create({
       amount,
-      currency: 'EUR',
+      currency: 'USD',
       confirm: true, // instantly confirms the payment. Defaults false.
       payment_method: token,
-    }).catch((err) => {
-      console.log(err);
-      throw new Error(err?.message || err)
     })
-    // 4. convert the cartItems to orderItems
-    // 5. create the order and return it 
-    return null;
-  }
-  
-  export default checkout;
-  
+    .catch((err) => {
+      console.log(err);
+      throw new Error(err?.message || err);
+    });
+  // 4. convert the cartItems to orderItems
+
+  console.log('====================================');
+  console.log(charge);
+  console.log('====================================');
+  // 5. create the order and return it
+  return null;
+}
+
+export default checkout;
