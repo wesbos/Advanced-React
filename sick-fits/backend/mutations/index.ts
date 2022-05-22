7d67b9a0c19be4741e949ca314368a62098fa06e
David Coelho <davidcoelho02@outlook.com>
2022-05-22 14:21:11 +0100

    feat: custom Checkout Mutation with Stripe - 55

--- sick-fits/backend/mutations/index.ts
+++ sick-fits/backend/mutations/index.ts
@@ -1,5 +1,6 @@
 import { graphQLSchemaExtension } from '@keystone-next/keystone/schema';
 import addToCart from './addToCart';
+import checkout from './checkout';
 
 // make a fake graphql tagged template literal > so we can see syntax hightliting
 const graphql = String.raw;
@@ -8,11 +9,13 @@ export const extendGraphqlSchema = graphQLSchemaExtension({
   typeDefs: graphql`
     type Mutation {
       addToCart(productId: ID): CartItem
+      checkout(token: String!): Order
     }
   `,
   resolvers: {
     Mutation: {
       addToCart,
+      checkout,
     },
   },
 });
