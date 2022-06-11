a4d94af1a850df4924f7b70cff609ca8b5e8756e
Andres Fresnedo <andres.fresnedo@slalom.com>
2022-06-11 12:01:50 -0700

    Frontend - add Page component

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,9 @@
+import Page from '../components/Page';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Page />
+    </div>
+  );
+}
--- sick-fits/frontend/pages/orders.js
+++ sick-fits/frontend/pages/orders.js
@@ -1,6 +1,9 @@
+import Page from '../components/Page';
+
 export default function OrderPage() {
   return (
     <div>
+      <Page />
       <p>testing eslint on orders page</p>
     </div>
   );
