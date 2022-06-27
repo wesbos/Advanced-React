ee01d44f750e6a97b184c9e1ce282dca06a5a0f2
David Coelho <davidcoelho02@outlook.com>
2022-06-27 22:39:53 +0100

    feat: Test Setup, Tooling and Methodology - 71

--- sick-fits/frontend/jest.setup.js
+++ sick-fits/frontend/jest.setup.js
@@ -1,3 +1,5 @@
 import '@testing-library/jest-dom';
 
+// since we using headless mode - which means, not opening an actual browser, using js-dom under the hood -
+// we don't actualy have access to the window object
 window.alert = console.log;
