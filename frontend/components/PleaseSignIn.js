ff6e37d1122c438c2cb4891fcf4443fb66f9a367
danriley11 <dan_riley7@outlook.com>
2023-11-21 13:48:16 +1100

    npm install and cleanup

--- frontend/components/PleaseSignIn.js
+++ frontend/components/PleaseSignIn.js
@@ -1,8 +1,10 @@
 import { useUser } from './User';
 import SignIn from './SignIn';
 
-export default function ({ children }) {
+const PleaseSignIn = ({ children }) => {
   const me = useUser();
   if (!me) return <SignIn />;
   return children;
-}
+};
+
+export default PleaseSignIn;
